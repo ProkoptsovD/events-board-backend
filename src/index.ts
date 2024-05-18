c3baffd143b0a7c3a96e0102993c5a039cec4340
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-05-18 20:43:40 +0300

    Fix cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.options('*', cors());
-app.use(cors({ origin: ['*', 'http://localhost:3000'] }));
+app.use(cors());
 app.use(express.json());
 
 app.use(eventsRouter);
