e9a0ff475645097221b4c5a797c78cc865a30e10
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-05-18 16:58:53 +0300

    [Feature] setup server

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import eventsRouter from './lib/routes/events.route';
+
+import { connectDb } from './lib/database/db';
+
+const { log } = console;
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(eventsRouter);
+
+app.listen(port, async () => {
+  log(`Example app listening on port ${port}`);
+
+  await connectDb().catch(console.log);
+  log('Connection to db established');
+});
